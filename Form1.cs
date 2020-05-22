e404527841f19abb9deb256eab7cd92477668516
Ingus Andris Pērkons <slaimy333@gmail.com>
2020-05-22 17:39:42 +0300

    I think there shouldnt be any bugs. I think its finished...

--- Form1.cs
+++ Form1.cs
@@ -13,15 +13,18 @@ namespace MemoryGame
 {
     public partial class Form1 : Form
     {
+        Label label1;
+        Label label2;
+        Timer labelTimer;
 
         Random rand = new Random();
-        List<string> icons = new List<string>()
-        {"S", "S", "D", "D", "F", "F", "G", "H", "J", "C", "M", "G", "H", "J", "C", "M" };
+
         public Form1()
         {
             InitializeComponent();
             InitializeGrid();
             PutRandomIcons();
+            InitializeTimer();
         }
 
         private void InitializeGrid()
@@ -30,7 +33,7 @@ namespace MemoryGame
             this.Width = 400;
             this.Height = 400;
 
-            tlayoutGrid.BackColor = Color.LimeGreen;
+            tlayoutGrid.BackColor = Color.Red;
             tlayoutGrid.CellBorderStyle = TableLayoutPanelCellBorderStyle.OutsetDouble;
 
             AddLabelsToGrid();
@@ -38,6 +41,55 @@ namespace MemoryGame
             //tlayoutGrid.Controls.Add(new Label { Text = "Type:", Anchor = AnchorStyles.Left, AutoSize = true }, 0, 0);
 
             //SDFGHJCB
+        }
+
+        private void InitializeTimer()
+        {
+            labelTimer = new Timer();
+            labelTimer.Interval = 1000;
+            labelTimer.Tick += new EventHandler(CheckIfEqual);
+
+        }
+
+        private void CheckIfEqual(object sender, EventArgs e)
+        {
+            if (label1.Text == label2.Text)
+            {
+                label1.ForeColor = Color.Green;
+                label2.ForeColor = Color.Green;
+                label1.Click -= Label_Click;
+                label2.Click -= Label_Click;
+                label1 = null;
+                label2 = null;
+            }
+            else
+            {
+                label1.ForeColor = tlayoutGrid.BackColor;
+                label2.ForeColor = tlayoutGrid.BackColor;
+                label1 = null;
+                label2 = null;
+            }
+            CheckWin();
+            labelTimer.Stop();
+        }
+
+        private void CheckWin()
+        {
+            int greenCount = 0;
+            foreach (Label label in tlayoutGrid.Controls)
+            {
+                if (label.ForeColor == Color.Green)
+                {
+                    greenCount++;
+
+                }
+            }
+            if (greenCount == 16)
+            {
+                MessageBox.Show("You Win!");
+                PutRandomIcons();
+            }
+
         }
 
         private void AddLabelsToGrid()
@@ -51,28 +103,59 @@ namespace MemoryGame
                     cellLabel = new Label
                     {
                         Dock = DockStyle.Fill,
-                        ForeColor = Color.Black,
                         AutoSize = false,
                         TextAlign = ContentAlignment.MiddleCenter,
-                        Text = " ",
-                        Font = new Font("Webdings", 72, FontStyle.Bold),
+                        Font = new Font("Webdings", 72, FontStyle.Bold),                       
                     };
                     tlayoutGrid.Controls.Add(cellLabel, c, r);
                 }
             }
         }
 
+        private void Label_Click(object sender, EventArgs e)
+        {
+            if(labelTimer.Enabled == true)
+            {
+                return;
+            }
+            Label label = (Label)sender;
+            if(label1 == null)
+            {
+                label1 = label;
+            }
+            else
+            {
+                label2 = label;
+            }
+            label.ForeColor = Color.Black;
 
+            if(label1 != null && label2 != null)
+            {
+                if(label1 == label2)
+                {
+                    label2 = null;
+                    return;
+                }
+                labelTimer.Start();
+
+
+            }
+
+        }
 
         private void PutRandomIcons()
         {
             Label label;
+            List<string> icons = new List<string>()
+        {"S", "S", "D", "D", "F", "F", "G", "H", "J", "C", "M", "G", "H", "J", "C", "M" };
 
-            for(int i = 0; i < 16; i++)
+            for (int i = 0; i < 16; i++)
             {
                 int randomIndex = rand.Next(0, icons.Count);
                 label = (Label)tlayoutGrid.Controls[i];
                 label.Text = icons[randomIndex];
+                label.ForeColor = tlayoutGrid.BackColor;
+                label.Click += new EventHandler(Label_Click);
                 icons.Remove(icons[randomIndex]);
             }
 
